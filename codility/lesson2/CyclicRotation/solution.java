e6d3ef6006e3409fa2d4c472f7cd072fd7646a94
JinHan <hjh5488@gmail.com>
2018-06-30 22:47:48 +0900

    Refactor indent 4 to 2

--- codility/lesson2/CyclicRotation/solution.java
+++ codility/lesson2/CyclicRotation/solution.java
@@ -5,19 +5,19 @@
 // System.out.println("this is a debug message");
 
 class Solution {
-    public int[] solution(int[] A, int K) {
-        if (K < 0 || K > 100) {
-          throw new IllegalArgumentException("invalid K");
-        }
-    
-        int[] result = new int[A.length];
-        final int length = A.length - 1;
-    
-        for(int i=0; i<=length; i++) {
-          final int newIndex = i + K < length ? i + K : (i + K) % (length + 1);
-          result[newIndex] = A[i];
-        }
-    
-        return result;
+  public int[] solution(int[] A, int K) {
+    if (K < 0 || K > 100) {
+      throw new IllegalArgumentException("invalid K");
     }
+
+    int[] result = new int[A.length];
+    final int length = A.length - 1;
+
+    for(int i=0; i<=length; i++) {
+      final int newIndex = i + K < length ? i + K : (i + K) % (length + 1);
+      result[newIndex] = A[i];
+    }
+
+    return result;
+  }
 }
--- codility/lesson2/CyclicRotation/solution.js
+++ codility/lesson2/CyclicRotation/solution.js
@@ -1,19 +1,19 @@
 // you can write to stdout for debugging purposes, e.g.
-// console.log('this is a debug message');
+// console.log('this is a debug message')
 
-function solution(A, K) {
-    // index + k < length ? index + k : (index + k) % (length + 1)
-    const length = A.length - 1
-    let result = []
-    
-    if (K < 0 || K > 100) {
-      throw new Error('invalid K')    
-    }
-    
-    A.forEach((v, i) => {
-      const newIndex = i + K < length ? i + K : (i + K) % (length + 1)
-      result[newIndex] = v
-    })
-    
-    return result
+function solution (A, K) {
+  // index + k < length ? index + k : (index + k) % (length + 1)
+  const length = A.length - 1
+  let result = []
+
+  if (K < 0 || K > 100) {
+    throw new Error('invalid K')
+  }
+
+  A.forEach((v, i) => {
+    const newIndex = i + K < length ? i + K : (i + K) % (length + 1)
+    result[newIndex] = v
+  })
+
+  return result
 }
